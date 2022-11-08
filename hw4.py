452bc981d342a702fb21c94080fec947c2d58dd0
Pearl Herrero <maryglen.herrero@bse.eu>
2022-11-08 16:06:17 +0100

    questions 1 4 7

--- hw4.py
+++ hw4.py
@@ -6,6 +6,8 @@
 # by three.
 #
 
+def triple(x):
+    return x * 3
 
 # 2)
 # Create a function named "subtract" that
@@ -49,12 +51,20 @@ def dictionary_maker(myTuple: list):
 # Create a function called "has_experience_as"
 # that has two parameters:
 # 1. A list of CV's.
-# 2. A string (job_title)
+# 2. A string (job_titlew)
 #
 # The function should return a list of strings
 # representing the usernames of every user that
 # has worked as job_title.
 
+def has_experience_as(cvs, job_title):
+    experienced_users = []
+    
+    for cv in cvs:
+        if job_title in cv['jobs']:
+            experienced_users.append(cv['user'])
+    
+    return experienced_users
 
 
 #
@@ -116,7 +126,8 @@ def most_popular_job(cvList: list):
 # The function should return the total number of cases
 # registered so far in that country
 
-
+def total_registered_cases(covid_count, country):
+      return sum(covid_count[country])
 # 8)
 # Create a function called "total_registered_cases_per_country"
 # that has 1 parameter:
